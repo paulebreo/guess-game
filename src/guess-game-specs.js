22712db5b35bfc0ed0a8b7feea03f83d5ca478e1
Paul Ebreo <paulebreo@triton.edu>
2018-08-29 19:01:11 -0500

    generate random number upon instantiation

--- src/guess-game-specs.js
+++ src/guess-game-specs.js
@@ -2,10 +2,16 @@ describe('GuessGame Class', () => {
     it('it attaches a constructor method to the GuessGame Class', () => {
         expect(typeof GuessGame.constructor).toBe('function');
     });
+    
+    it('constructor should generate random number between 1 to 100 if no number given', () => {
+        const game = new GuessGame();
+        const number = game.secretNumber
+        let isBetween = (number >= 0) && (number <= 100) 
+        expect(isBetween).toBe(true);
+    });
 
     it('the instance has `secretNumber`, `remainingGuesses`, `numbersGuessed`, and `gameState` properties', () => {
         const game = new GuessGame(46);
-
         expect(game.secretNumber).toEqual(46);
         expect(game.remainingGuesses).toBe(5);
         expect(game.numbersGuessed).toEqual([]);
@@ -25,6 +31,8 @@ describe('GuessGame Class', () => {
             expect(game.hasOwnProperty('computeGameState')).toBe(false);
         });
     });
+
+    
 
     
 });
--- src/guess-game.js
+++ src/guess-game.js
@@ -1,9 +1,11 @@
+const getRandomInt = (min, max) =>  Math.floor(Math.random() * (Math.ceil(max) - Math.ceil(min))) + min
+
 class GuessGame {
     constructor(number) {
-        this.secretNumber = number || undefined;
-        this.numbersGuessed = []
-        this.remainingGuesses = 5
-        this.gameState = 'playing';
+      this.secretNumber = number || getRandomInt(1, 100);
+      this.numbersGuessed = [];
+      this.remainingGuesses = 5;
+      this.gameState = 'playing';
     }
     submitGuess(guess) {}
     computeGameState() {}
@@ -11,5 +13,5 @@ class GuessGame {
 
 class Game {}
 
-Game.prototype.setupGuessEvents();
-Game.prototype.start();
+// Game.prototype.setupGuessEvents();
+// Game.prototype.start();
